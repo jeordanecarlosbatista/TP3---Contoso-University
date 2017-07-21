ea79edac1aec340b299d448aeba32aef462149be
Jeordane Batista <jeordane.batista@hotmail.com>
2017-07-21 16:54:39 -0300

    Initial project

---
+++ ASP.NET MVC Application Using Entity Framework Code First/C#/ContosoUniversity/Models/Partner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Partner
+    {
+        public int PartnerID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
